015f4042d948aedbd828fab4137cb9fba5012700
Franco Dionofrio <83983474+Noversal@users.noreply.github.com>
2024-04-07 17:47:16 +0000

    Remove unnecessary providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { FilterBookProvider } from './context/bookfilter'
-import { BookLikeProvider } from './context/bookLike'
 import AllBooks from './pages/AllBooks'
 import BooksLiked from './pages/BooksLiked'
 import Layout from './components/Layout'
@@ -10,8 +8,8 @@ function App () {
   return (
 <BrowserRouter>
       <Routes>
-        <Route path='/' element={<BookLikeProvider><Layout/></BookLikeProvider>}>
-          <Route index element={<FilterBookProvider><AllBooks /></FilterBookProvider>} />
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<AllBooks />} />
           <Route path='/likes' element={<BooksLiked/>} />
         </Route>
   </Routes>
